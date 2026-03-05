27fd4caac5e19827d2d681e8055e80e4da9f6470
sachinrkumar1000-maker <sachinrkumar1000@gmail.com>
2026-03-06 00:21:50 +0530

    Create register.js

---
+++ netlify/Functions/register.js
@@ -0,0 +1,31 @@
+// netlify/functions/register.js
+// This runs on Netlify's servers - no CORS or SSL issues!
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+
+    const response = await fetch('http://paid2.daki.cc:4156/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(result),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
